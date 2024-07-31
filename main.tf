ea4b622e8ee869307b4999792fd7b83d9c420dd6
Haripalepu <harikpalepu@gmail.com>
2024-07-31 13:05:08 +0530

    updating

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
   enable_dns_hostnames = var.enable_dns_hostnames
-  tags = merge(                              #can merge multiple maps 
+  tags = merge(                              #can merge multiple maps.
     var.common_tags, 
     var.vpc_tags,
   {
